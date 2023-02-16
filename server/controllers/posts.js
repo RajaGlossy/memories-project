4d4fe4c341faf36d65f349b3d5d633cd180dea90
Raja <glossyraj97@gmail.com>
2023-02-16 15:22:18 +0530

    first commit

---
+++ server/controllers/posts.js
@@ -0,0 +1,15 @@
+import PostMessage from "../modules/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPosts = (req, res) => {
+  res.send("post created successfully");
+};
